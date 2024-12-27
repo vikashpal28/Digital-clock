ef6fc2b73ffad67cda8e55bc3cc0ae505c3ef693
Vikash pal <151984787+vikashpal28@users.noreply.github.com>
2024-12-27 18:46:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,35 @@ function clock(){
     minutes.innerHTML = m;
     seconds.innerHTML = s;
 }
+
+
+function week(){
+    let w = document.querySelector('.w')
+    let d = document.querySelector('.d')
+    let pman = document.querySelector('.pmam')
+    let date = new Date();
+    let day = date.getDay();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let week = days[day];
+    w.innerHTML = week.toUpperCase();
+    let month = date.getMonth();
+    let months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12'];
+    let mon = months[month];
+    let year = date.getFullYear();
+    let dat = date.getDate();
+    d.innerHTML = dat + '/' + mon + '/' + year;
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let ampm = h >= 12 ? 'PM' : 'AM';
+    pman.innerHTML = ampm.toUpperCase();
+
+}
+
+week();
+
+
+
 let interval = setInterval(clock, 1000);
 
 let container = document.querySelector('.container');
